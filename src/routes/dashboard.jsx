e636172c1e75cec938163fac63dcfa87364e321c
nadun94 <nadunsirimevan@gmail.com>
2018-10-05 03:43:04 +0530

    fee combination

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,6 +1,7 @@
 import Home from "views/Home/Home.jsx";
 import Fee from '../views/Fee/Fee.jsx';
 import Passenger from "views/Passenger/Passenger.jsx";
+import BusStopDistance from '../views/BusStopDistance/BusStopDistance.jsx';
 var dashRoutes = [
   {
     path: "/home",
@@ -16,6 +17,11 @@ var dashRoutes = [
     path: "/passenger",
     name: "Passenger",
     component: Passenger
+  },
+  {
+    path: "/bus-stop-distance",
+    name: "Bus Stop Distance",
+    component: BusStopDistance
   },
 
   { redirect: true, path: "/", pathTo: "/home", name: "Home" }
