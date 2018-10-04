2c63ec6e67bbf2ca9154529ce06321dfb081c1bf
C_Jay <chandirajayasekara@gmail.com>
2018-10-04 11:54:49 +0530

    Passenger Handling front end initial commit

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,12 +1,18 @@
 import Home from "views/Home/Home.jsx";
+import Passenger from "views/Passenger/Passenger.jsx";
 
 var dashRoutes = [
-  {
-    path: "/home",
-    name: "Home",
-    component: Home
-  },
- 
-  { redirect: true, path: "/", pathTo: "/home", name: "Home" }
+    {
+        path: "/home",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/passenger",
+        name: "Passenger",
+        component: Passenger
+    },
+
+    {redirect: true, path: "/", pathTo: "/home", name: "Home"}
 ];
 export default dashRoutes;
