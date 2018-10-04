a0866d1920d8b1240b611a4c989550ed55638eb9
nadun94 <nadunsirimevan@gmail.com>
2018-10-05 00:39:58 +0530

    ffe

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,10 +1,15 @@
 import Home from "views/Home/Home.jsx";
-
+import Fee from '../views/Fee/Fee.jsx';
 var dashRoutes = [
   {
     path: "/home",
     name: "Home",
     component: Home
+  },
+  {
+    path:"/fee",
+    name:"Fee",
+    component:Fee
   },
  
   { redirect: true, path: "/", pathTo: "/home", name: "Home" }
